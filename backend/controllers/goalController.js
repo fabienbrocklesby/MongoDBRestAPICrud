d55242c40bd8999d02c70176edcfde8ab6de1b03
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2022-02-12 12:26:25 +1300

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,11 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalModel');
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
+    const goals = await Goal.find();
+
+    res.status(200).json(goals)
 });
 
 const setGoal = asyncHandler(async (req, res) => {
@@ -10,15 +14,39 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({ message: 'Set goal' })
+    const goal = await Goal.create({
+        text: req.body.text
+    });
+
+    res.status(200).json(goal)
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+
+    res.status(200).json(updatedGoal)
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove();
+
+    res.status(200).json({ id: req.params.id })
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 
 const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
 const port = process.env.PORT || 5000
+
+connectDB();
 
 const app = express()
 
